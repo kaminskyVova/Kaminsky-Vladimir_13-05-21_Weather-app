492e8e78f677e89387e4e6416459fe3a14664345
Kaminsky <kaminskyfrontend@gmail.com>
2021-05-15 16:35:54 +0300

    Updates

---
+++ precache-manifest.6488ca53edd96a36af22e894c9f22627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4053a76577e7cc55a26f810d82aad1b1",
+    "url": "/Kaminsky-Vladimir_13-05-21_Weather-app/index.html"
+  },
+  {
+    "revision": "27925b2799d4f7d47739",
+    "url": "/Kaminsky-Vladimir_13-05-21_Weather-app/static/css/main.58f5288c.chunk.css"
+  },
+  {
+    "revision": "6f5a79c1edf52bb05ce9",
+    "url": "/Kaminsky-Vladimir_13-05-21_Weather-app/static/js/2.10d8d463.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Kaminsky-Vladimir_13-05-21_Weather-app/static/js/2.10d8d463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27925b2799d4f7d47739",
+    "url": "/Kaminsky-Vladimir_13-05-21_Weather-app/static/js/main.dc0da20c.chunk.js"
+  },
+  {
+    "revision": "3c5f8a7f138c17f734ba",
+    "url": "/Kaminsky-Vladimir_13-05-21_Weather-app/static/js/runtime-main.4c0bfc98.js"
+  }
+]);
